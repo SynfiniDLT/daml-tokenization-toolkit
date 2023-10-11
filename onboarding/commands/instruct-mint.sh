16dc7ef7dfdcbdc52ac18aa46ef1e9c854bf2e00
hgrano <huw.grano@gmail.com>
2023-10-11 15:07:54 +1100

    remove variable

--- onboarding/commands/instruct-mint.sh
+++ onboarding/commands/instruct-mint.sh
@@ -2,9 +2,8 @@
 
 set -eu
 
-reference_id=$(uuidgen)
 $TOKENIZATION_UTIL/add-json.sh $TOKENIZATION_PARTIES_FILE $1 | \
-  jq --arg referenceId $reference_id '. * {"instructMintSettings": {"referenceId": $referenceId}}' | \
+  jq --arg referenceId $(uuidgen) '. * {"instructMintSettings": {"referenceId": $referenceId}}' | \
   $TOKENIZATION_UTIL/daml-script.sh \
   --input-file /dev/stdin \
   --dar ${TOKENIZATION_ONBOARDING_DAR} \
