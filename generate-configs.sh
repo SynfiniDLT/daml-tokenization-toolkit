e5108ee1c7a1994f95f4c3931e50510f8d913c95
Alfred Xia <alfred.xia@asx.com.au>
2023-11-20 14:35:17 +1100

    install aws cli

--- generate-configs.sh
+++ generate-configs.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Installing aws cli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+    && unzip awscliv2.zip \
+    && ./aws/install \
+    && rm awscliv2.zip \
+    && rm -R aws
+echo "Done."
+
+
 echo "Installing docker-compose"
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
