0603797f5494989ffa7d03ebaf24bd9897377258
Alfred Xia <alfred.xia@asx.com.au>
2023-11-20 14:34:54 +1100

    add ecr login

--- start-containers.sh
+++ start-containers.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo $PWD
-cd $PWD
-echo $PWD
 cd /home/ec2-user/alfred/synfini-wallet-app-deploy
 echo $PWD
+
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com
+
 docker-compose up -d
