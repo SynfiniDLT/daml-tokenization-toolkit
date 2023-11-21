fab60e1e2a9dfd9ceec297c1e6ef06344193e3ea
EC2 Default User <ec2-user@ip-100-64-95-77.ap-southeast-2.compute.internal>
2023-11-21 03:20:21 +0000

    adding docker login to ECR in the start containers script

--- start-containers.sh
+++ start-containers.sh
@@ -1,6 +1,6 @@
 
 
-
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com
 
 docker pull 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com/synfini-wallet-be:0.0.1
 
