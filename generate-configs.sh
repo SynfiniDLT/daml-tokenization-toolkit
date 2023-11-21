3a5d6b4f6935c7321ee05cdd51080051af91987b
EC2 Default User <ec2-user@ip-100-64-95-77.ap-southeast-2.compute.internal>
2023-11-21 03:04:10 +0000

    testing front-end deploy

--- generate-configs.sh
+++ generate-configs.sh
@@ -3,5 +3,7 @@
 
 aws s3 cp s3://dlts-artifacts/synfini-wallet-app/canton-dev-dlt02/application.properties /opt/app/config
 
-echo "application properties file downloaded"
+aws s3 cp s3://dlts-artifacts/synfini-wallet-app/canton-dev-dlt02/.env /opt/app/config
+
+echo "config files downloaded"
 
--- start-containers.sh
+++ start-containers.sh
@@ -4,6 +4,12 @@
 
 docker pull 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com/synfini-wallet-be:0.0.1
 
+docker pull 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com/synfini-wallet-fe:0.0.1
+
 docker rm -f synfini-wallet-backend
 
+docker rm -f synfini-wallet-frontend
+
 docker run -v /opt/app/config:/app/config -p 8091:8091 --name synfini-wallet-backend --rm -d -it 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com/synfini-wallet-be:0.0.1
+
+docker run -v /opt/app/config:/app/config -p 8091:8091 --name synfini-wallet-frontend --rm -d -it 115676289457.dkr.ecr.ap-southeast-2.amazonaws.com/synfini-wallet-fe:0.0.1
