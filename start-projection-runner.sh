5293aa154b387953f3c08fe74c2504bd663db6f5
Alfred Xia <alfred.xia@asx.com.au>
2023-11-24 16:08:47 +1100

    use localhost to start projection runner in script

--- start-projection-runner.sh
+++ start-projection-runner.sh
@@ -3,7 +3,7 @@
 DEPLOYMENT_GROUP_NAME="dev"
 if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
 then
-    projection_url="http://0.0.0.0:8091/wallet-views/v1/projection/start"
+    projection_url="http://localhost:8091/wallet-views/v1/projection/start"
     token_url="https://asx-dev.au.auth0.com/oauth/token"
     secret_name="Dev_Auth0_dlt02_wallet_operator"
     party_wallet_operator="WalletServiceProvider_Wallet_Operator_v1::12205d4208edf13d0cfc27a8f0cf05ff3cb18d8b437ab70c36c8d7a12e51a06b781d"
