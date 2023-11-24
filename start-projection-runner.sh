e196ebb59a4f452294079cc26364c93961aac103
Alfred Xia <alfred.xia@asx.com.au>
2023-11-24 13:40:57 +1100

    add script to start projection runner, update appspec as well

---
+++ start-projection-runner.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+DEPLOYMENT_GROUP_NAME="dev"
+if [ "$DEPLOYMENT_GROUP_NAME" == "dev" ]
+then
+    projection_url="https://wallet.dlt.canton.dltsolutions.com.au/wallet-views/v1/projection/start"
+    token_url="https://asx-dev.au.auth0.com/oauth/token"
+    secret_name="Dev_Auth0_dlt02_wallet_operator"
+    party_wallet_operator="WalletServiceProvider_Wallet_Operator_v1::12205d4208edf13d0cfc27a8f0cf05ff3cb18d8b437ab70c36c8d7a12e51a06b781d"
+fi
+
+token_secret_val=$(
+    aws secretsmanager get-secret-value \
+    --secret-id ${secret_name} | jq -r '.SecretString'
+)
+client_id=$(echo $token_secret_val | jq -r '.client_id')
+client_secret=$(echo $token_secret_val | jq -r '.client_secret')
+audience=$(echo $token_secret_val | jq -r '.audience')
+# echo "token_secret_val=${token_secret_val}"
+# echo "client_id=${client_id}"
+# echo "client_secret=${client_secret}"
+# echo "audience=${audience}"
+
+cmd="
+    curl -so /dev/null -w \"%{http_code}\" \
+        --location \"${projection_url}\" \
+        --header \"Content-Type: application/json\" \
+        --data '{
+          \"readAs\": \"${party_wallet_operator}\",
+          \"tokenUrl\": \"${token_url}\",
+          \"clientId\": \"${client_id}\",
+          \"clientSecret\": \"${client_secret}\",
+          \"audience\": \"${audience}\"
+        }'
+"
+echo ${cmd}
+response_code=$(eval "${cmd}")
+echo "response_code=${response_code}"
+if [ ${response_code} == "200" ]
+then
+    echo "Projection Runner started successfully!"
+else
+    echo "ERROR while starting Projection Runner!"
+fi
