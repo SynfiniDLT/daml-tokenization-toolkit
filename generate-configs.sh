b86db40937e80555a7ddea441922bd9d66ed7aa2
fabio <fabio.silvadacosta@asx.com.au>
2023-12-01 14:57:17 +1100

    PR comments

--- generate-configs.sh
+++ generate-configs.sh
@@ -1,5 +1,4 @@
-
-
+#!/bin/bash
 
 aws s3 cp s3://dlts-artifacts/synfini-wallet-app/canton-dev-dlt02/application.properties /opt/app/config
 
--- wallet-ui/src/components/layout/copyToClipboard.tsx
+++ wallet-ui/src/components/layout/copyToClipboard.tsx
@@ -1,38 +1,48 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { Clipboard, ClipboardCheck } from "react-bootstrap-icons";
 
-export default function CopyToClipboard(props: { paramToCopy:any, paramToShow:any }) {
-
-    const [toggleClipboard, setToggleClipboard] = useState(false);
-  
-    const copyContentClipboard = (param: string) => {
-      navigator.clipboard.writeText(param)
-      setToggleClipboard(!toggleClipboard);
-    }
-  
-
-  return (
-    <span onClick={() => copyContentClipboard(props.paramToCopy)}>
-        {props.paramToShow} {" "}
-        {!toggleClipboard ? <Clipboard /> : <><ClipboardCheck /><span> copied!</span></>}
-    </span>
-  );
-};
-
-export function CopyToClipboardFromPopUp(props: { paramToCopy:any }) {
-
+export default function CopyToClipboard(props: { paramToCopy: any; paramToShow: any }) {
   const [toggleClipboard, setToggleClipboard] = useState(false);
 
   const copyContentClipboard = (param: string) => {
-    navigator.clipboard.writeText(param)
+    navigator.clipboard.writeText(param);
     setToggleClipboard(!toggleClipboard);
-  }
+  };
 
+  return (
+    <span onClick={() => copyContentClipboard(props.paramToCopy)}>
+      {props.paramToShow}{" "}
+      {!toggleClipboard ? (
+        <Clipboard />
+      ) : (
+        <>
+          <ClipboardCheck />
+          <span> copied!</span>
+        </>
+      )}
+    </span>
+  );
+}
 
-return (
-  <span onClick={() => copyContentClipboard(props.paramToCopy)}>
-    {props.paramToCopy} {" "} {!toggleClipboard ? <Clipboard /> : <><ClipboardCheck /><span> copied!</span></>}
-  </span>
-);
-};
+export function CopyToClipboardFromPopUp(props: { paramToCopy: any }) {
+  const [toggleClipboard, setToggleClipboard] = useState(false);
 
+  const copyContentClipboard = (param: string) => {
+    navigator.clipboard.writeText(param);
+    setToggleClipboard(!toggleClipboard);
+  };
+
+  return (
+    <span onClick={() => copyContentClipboard(props.paramToCopy)}>
+      {props.paramToCopy}{" "}
+      {!toggleClipboard ? (
+        <Clipboard />
+      ) : (
+        <>
+          <ClipboardCheck />
+          <span> copied!</span>
+        </>
+      )}
+    </span>
+  );
+}
