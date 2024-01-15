44f78b55c66f35bbd9b2043ccfba2f3573961da4
hgrano <huw.grano@gmail.com>
2024-01-15 13:56:55 +1100

    getting ui to compile with new daml model

--- wallet-ui/src/pages/InstrumentCreateFormScreen.tsx
+++ wallet-ui/src/pages/InstrumentCreateFormScreen.tsx
@@ -13,7 +13,7 @@ import {
 } from "../components/layout/general.styled";
 import { Party, emptyMap, Map } from "@daml/types";
 import { packageStringFromParty } from "../components/Util";
-import { Issuer as TokenIssuer } from "@daml.js/issuer-onboarding-token-interface/lib/Synfini/Interface/Onboarding/Issuer/Token/Issuer";
+import { Issuer as TokenIssuer } from "@daml.js/issuer-onboarding-instrument-token-interface/lib/Synfini/Interface/Onboarding/Issuer/Instrument/Token/Issuer";
 import { Set } from "@daml.js/97b883cd8a2b7f49f90d5d39c981cf6e110cf1f1c64427a28a6d58ec88c43657/lib/DA/Set/Types";
 import { v4 as uuid } from "uuid";
 import { WalletViewsClient } from "@synfini/wallet-views";
