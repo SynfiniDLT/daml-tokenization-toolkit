c34d32408a3a874e38c599053ac95f68c7616d65
hgrano <huw.grano@gmail.com>
2024-02-26 16:51:24 +1100

    removing unused scripts

--- onboarding/main.sh
+++ onboarding/main.sh
@@ -55,10 +55,6 @@ export DOPS_ACCOUNT_OPEN_OFFERS_FILE=${DOPS_INTERNAL}/account-open-offers.json
 if [ ! -f "${DOPS_ACCOUNT_OPEN_OFFERS_FILE}" ]; then
   echo '{"accountOpenOffers": []}' > "${DOPS_ACCOUNT_OPEN_OFFERS_FILE}"
 fi
-export DOPS_FUND_ISSUER_OPEN_OFFERS_FILE=${DOPS_INTERNAL}/fund-issuer-open-offers.json
-if [ ! -f "${DOPS_FUND_ISSUER_OPEN_OFFERS_FILE}" ]; then
-  echo '{"fundIssuerOpenOffers": []}' > "${DOPS_FUND_ISSUER_OPEN_OFFERS_FILE}"
-fi
 
 if [ -z ${1+x} ]; then
   echo "Please provide command"
@@ -146,16 +142,6 @@ elif [ "$command" = "create-issuers" ]; then
   ${dops_commands}/create-issuers.sh $@
 elif [ "$command" = "create-minter-burners" ]; then
   ${dops_commands}/create-minter-burners.sh $@
-elif [ "$command" = "create-fund-offer-unilateral" ]; then
-  ${dops_commands}/create-fund-offer.sh $@
-elif [ "$command" = "create-fund-issuer-open-offers" ]; then
-  ${dops_commands}/create-fund-issuer-open-offers.sh $@
-elif [ "$command" = "take-fund-issuer-open-offer" ]; then
-  ${dops_commands}/take-open-fund-issuer-offer.sh $@
-elif [ "$command" = "create-fund-investors" ]; then
-  ${dops_commands}/create-fund-investors.sh $@
-elif [ "$command" = "accept-fund-purchase" ]; then
-  ${dops_commands}/accept-fund-purchase.sh $@
 else
   echo "Unsupported command: $command"
   exit 1
