61cf3e214bcfc5987f33a395bbe4d17b1683734b
hgrano <huw.grano@gmail.com>
2024-05-08 09:52:15 +1000

    fix up offer accept form to allow revoking and rejecting

--- wallet-ui/src/pages/OfferAcceptFormScreen.tsx
+++ wallet-ui/src/pages/OfferAcceptFormScreen.tsx
@@ -3,10 +3,6 @@ import { PageLayout } from "../components/PageLayout";
 import { useWalletUser, userContext } from "../App";
 import { useLocation, useNavigate } from "react-router-dom";
 import {
-  ContainerColumn,
-  ContainerColumnAutoField,
-  ContainerColumnField,
-  ContainerDiv,
   DivBorderRoundContainer,
 } from "../components/layout/general.styled";
 import { OneTimeOffer } from "@daml.js/synfini-settlement-one-time-offer-interface/lib/Synfini/Interface/Settlement/OneTimeOffer/OneTimeOffer";
@@ -14,7 +10,6 @@ import Modal from "react-modal";
 import { CreateEvent } from "@daml/ledger";
 import { repairMap, setToArray } from "../Util";
 import { InstrumentKey } from "@daml.js/daml-finance-interface-types-common/lib/Daml/Finance/Interface/Types/Common/Types";
-import { Step } from "@daml.js/daml-finance-interface-settlement/lib/Daml/Finance/Interface/Settlement/Types";
 
 type OfferAcceptFormScreenState = {
   offer: CreateEvent<OneTimeOffer, undefined, string>
@@ -30,15 +25,17 @@ export const OfferAcceptFormScreen: React.FC = () => {
   const [transactionRefInput, setTransactionRefInput] = useState("");
   const defaultQuantity = state.offer.payload.minQuantity || "1";
   const [quantityInput, setQuantityInput] = useState(defaultQuantity);
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [message, setMessage] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [accepted, setAccepted] = useState(false);
+
+  const isOfferee = primaryParty === state.offer.payload.offeree;
+  const isOfferer = primaryParty !== undefined && state.offer.payload.offerers.map.has(primaryParty);
 
   const handleInstrumentClick = (instrument: InstrumentKey) => {
     nav("/asset", { state: { instrument } });
   };
-
 
   const handleTransactionRef: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setTransactionRefInput(event.target.value);
@@ -51,7 +48,11 @@ export const OfferAcceptFormScreen: React.FC = () => {
   const handleCloseModal = () => {
     setIsModalOpen(!isModalOpen);
     if (error === "") {
-      nav("/settlements", { state: { transactionId: state.offer.payload.offerId.unpack } });
+      if (accepted) {
+        nav("/settlements", { state: { transactionId: state.offer.payload.offerId.unpack } });
+      } else {
+        nav("/offers");
+      }
     } else {
       nav("/offer/accept", { state: { offer: state.offer } });
     }
@@ -59,34 +60,57 @@ export const OfferAcceptFormScreen: React.FC = () => {
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    setIsSubmitting(true);
 
-    await ledger
-      .exercise(
+    try {
+      await ledger.exercise(
         OneTimeOffer.Accept,
         state.offer.contractId,
         {
           quantity: quantityInput,
           description: transactionRefInput,
         }
-      )
-      .then((resp) => {
-        if (resp[0]._2 !== undefined) {
-          setError("");
-          setMessage("Offer Accepted with success!");
-        } else {
-          setError("Error! Contract was not created. Please contact the admin");
-        }
-        setIsModalOpen(true);
-      })
-      .catch((err) => {
-        setError("Error! Offer Not Accepted \n \n Error:" + JSON.stringify(err.errors[0]));
-        setIsModalOpen(true);
-      })
-      .finally(() => {
-        setIsSubmitting(false);
-      });
+      );
+      setAccepted(true);
+      setError("");
+      const message = isOfferee ? "accepted" : "removed";
+      setMessage(`Request ${message}`);
+      setIsModalOpen(true);
+    } catch (err) {
+      setError("Sorry that didn't work");
+      console.log("Error submitting form", err);
+      setIsModalOpen(true);
+    }
   };
+
+  const handleReject: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+
+    try {
+      await ledger.exercise(OneTimeOffer.Reject, state.offer.contractId, {});
+      setError("");
+      setMessage("Request rejected");
+      setIsModalOpen(true);
+    } catch(err) {
+      setError("Sorry that didn't work");
+      console.log("Error submitting form", err);
+      setIsModalOpen(true);
+    }
+  }
+
+  const handleRemove: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+
+    try {
+      await ledger.exercise(OneTimeOffer.Revoke, state.offer.contractId, {});
+      setError("");
+      setMessage("Request removed");
+      setIsModalOpen(true);
+    } catch(err) {
+      setError("Sorry that didn't work");
+      console.log("Error submitting form", err);
+      setIsModalOpen(true);
+    }
+  }
 
   function stepElements(delivery: boolean) {
     const stepValues = state
@@ -97,7 +121,7 @@ export const OfferAcceptFormScreen: React.FC = () => {
       .map((step, index) =>
         <div key={index}>
           <a onClick={_ => handleInstrumentClick(step.quantity.unit)}>
-            {parseFloat(quantityInput) * parseFloat(step.quantity.amount)} {step.quantity.unit.id.unpack} {step.quantity.unit.version}
+            {(fixedAmount || isOfferee) && parseFloat(quantityInput) * parseFloat(step.quantity.amount)} {step.quantity.unit.id.unpack} {step.quantity.unit.version}
           </a>
           {delivery ? ` to ${step.receiver}` : ` from ${step.sender}`}
         </div>
@@ -114,45 +138,47 @@ export const OfferAcceptFormScreen: React.FC = () => {
       <h3 className="profile__title">Settlement Request</h3>
       <DivBorderRoundContainer style={{ height: "auto" }}>
         <form onSubmit={handleSubmit}>
-          {/* <ContainerDiv style={{ height: "200px" }}> */}
-            <table className="request">
+          <table className="request">
+            <tr>
+              <th>ID:</th>
+              <td>{state.offer.payload.offerId.unpack}</td>
+            </tr>
+            <tr>
+              <th>Description:</th>
+              <td>{state.offer.payload.offerDescription}</td>
+            </tr>
+            <tr>
+              <th>Requested by:</th>
+              <td>{setToArray(state.offer.payload.offerers).map(party => <>{party}<br/></>)}</td>
+            </tr>
+            <tr>
+              <th>Receivable:</th>
+              <td>{stepElements(false)}</td>
+            </tr>
+            <tr>
+              <th>Deliverable:</th>
+              <td>{stepElements(true)}</td>
+            </tr>
+            {
+              !fixedAmount && isOfferee &&
               <tr>
-                <th>ID:</th>
-                <td>{state.offer.payload.offerId.unpack}</td>
+                <th>Select amount:</th>
+                <td>
+                  <input
+                    type="number"
+                    name="quantity"
+                    style={{ width: "100px" }}
+                    onChange={handleQuantityInput}
+                    required
+                    min={state.offer.payload.minQuantity || "0"}
+                    max={state.offer.payload.maxQuantity || undefined}
+                    defaultValue={defaultQuantity}
+                  />
+                </td>
               </tr>
-              <tr>
-                <th>Description:</th>
-                <td>{state.offer.payload.offerDescription}</td>
-              </tr>
-              <tr>
-                <th>Requested by:</th>
-                <td>{setToArray(state.offer.payload.offerers).map(party => <>{party}<br/></>)}</td>
-              </tr>
-              <tr>
-                <th>Receivable:</th>
-                <td>{stepElements(false)}</td>
-              </tr>
-              <tr>
-                <th>Deliverable:</th>
-                <td>{stepElements(true)}</td>
-              </tr>
-              {!fixedAmount &&
-                <tr>
-                  <th>Select amount:</th>
-                  <td>
-                    <input
-                      type="number"
-                      name="quantity"
-                      style={{ width: "100px" }}
-                      onChange={handleQuantityInput}
-                      required
-                      min={state.offer.payload.minQuantity || "0"}
-                      max={state.offer.payload.maxQuantity || undefined}
-                      defaultValue={defaultQuantity}
-                    />
-                  </td>
-                </tr>
-              }
+            }
+            {
+              isOfferee &&
               <tr>
                 <th>Reference:</th>
                 <td>
@@ -165,11 +191,26 @@ export const OfferAcceptFormScreen: React.FC = () => {
                   />
                 </td>
               </tr>
-            </table>
+            }
+          </table>
+          {
+            isOfferee &&
             <button type="submit" className="button__login" style={{ width: "150px" }}>
               Accept
             </button>
-          {/* </ContainerDiv> */}
+          }
+          {
+            isOfferee &&
+            <button type="button" className="button__login" style={{ width: "150px" }} onClick={handleReject}>
+              Reject
+            </button>
+          }
+          {
+            primaryParty !== undefined && isOfferer &&
+            <button type="button" className="button__login" style={{ width: "150px" }} onClick={handleRemove}>
+              Remove
+            </button>
+          }
         </form>
       </DivBorderRoundContainer>
       <Modal
